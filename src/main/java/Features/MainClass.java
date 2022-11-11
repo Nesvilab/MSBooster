7c5c05e7f1c694ea5cd5b9f1a8919aafd0c4e6f7
yangkl96 <31668253+yangkl96@users.noreply.github.com>
2022-11-11 14:40:30 -0500

    bump to msbooster v1.1.7 for fragpipe v19.0

--- src/main/java/Features/MainClass.java
+++ src/main/java/Features/MainClass.java
@@ -10,7 +10,7 @@ import java.util.*;
 //this is what I use in the java jar file
 public class MainClass {
     public static void main(String[] args) throws Exception {
-        System.out.println("MSBooster v1.1.6");
+        System.out.println("MSBooster v1.1.7");
         try {
             //accept command line inputs
             HashSet<String> fields = new HashSet<>();

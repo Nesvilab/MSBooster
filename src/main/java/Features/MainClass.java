e9efb58b6f6db2b43fefebf51d4d69b1959404c7
yangkl96 <yangkl@umich.edu>
2024-01-30 12:45:19 -0500

    push to v1.2.0

--- src/main/java/Features/MainClass.java
+++ src/main/java/Features/MainClass.java
@@ -32,7 +32,7 @@ public class MainClass {
     public static ScheduledThreadPoolExecutor executorService;
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-        System.out.println("MSBooster v1.1.39-koina");
+        System.out.println("MSBooster v1.2.0");
 
         try {
             //accept command line inputs

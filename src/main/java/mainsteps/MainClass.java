e7ec3b7fc690f73c3425e712e98e5022dc6b56b2
Fengchao <fcyucn@gmail.com>
2024-12-12 11:18:17 -0500

    Bump to 1.2.63

--- src/main/java/mainsteps/MainClass.java
+++ src/main/java/mainsteps/MainClass.java
@@ -70,7 +70,7 @@ public class MainClass {
     public static ScheduledThreadPoolExecutor executorService;
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-        printInfo("MSBooster v1.2.62");
+        printInfo("MSBooster v1.2.63");
 
         try {
             //accept command line inputs

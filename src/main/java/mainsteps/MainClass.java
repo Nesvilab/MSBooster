398d6d38df501d908ce0e4bda732d9cf28f5b950
yangkl96 <yangkl@umich.edu>
2025-03-17 11:04:27 -0400

    bump to 1.3.0

--- src/main/java/mainsteps/MainClass.java
+++ src/main/java/mainsteps/MainClass.java
@@ -77,7 +77,7 @@ public class MainClass {
     public static ScheduledThreadPoolExecutor executorService;
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-        printInfo("MSBooster v1.2.73");
+        printInfo("MSBooster v1.3.0");
 
         try {
             //accept command line inputs

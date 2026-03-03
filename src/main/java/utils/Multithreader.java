4e04f0315e3680e9fe77da51c1e89b729e34db7b
yangkl96 <yangkl@umich.edu>
2026-03-03 13:38:50 -0500

    bump to v1.4.29. Multithreading fix

--- src/main/java/utils/Multithreader.java
+++ src/main/java/utils/Multithreader.java
@@ -5,9 +5,11 @@ public class Multithreader {
     //create make an array of equally sized entry subsets
     public Multithreader(int entries, int divisions) {
         indices = new int[divisions + 1];
-        int subsetSize = (int) (entries / (float) divisions);
-        for (int i = 0; i < divisions; i++) {
-            indices[i] = subsetSize * i;
+        int subsetSize = entries / divisions;
+        int remainder = entries % divisions;
+        indices[0] = 0;
+        for (int i = 1; i < divisions; i++) {
+            indices[i] = indices[i - 1] + subsetSize + (i - 1 < remainder ? 1 : 0);
         }
         indices[divisions] = entries;
     }

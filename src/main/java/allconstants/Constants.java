1126d236995db08d78734978f210ddd818a49adb
Fengchao <fcyucn@gmail.com>
2026-08-13 15:55:49 -0400

    Bump to 1.5.4

--- src/main/java/allconstants/Constants.java
+++ src/main/java/allconstants/Constants.java
@@ -25,7 +25,7 @@ import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Constants implements ConstantsInterface {
-    public static String versionNumber = "MSBooster v1.5.3";
+    public static String versionNumber = "MSBooster v1.5.4";
 
     //file input
     public static String paramsList = null;
